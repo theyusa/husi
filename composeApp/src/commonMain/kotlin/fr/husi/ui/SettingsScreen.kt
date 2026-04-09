072ec750b78121a8e80d638dfc7e5c1d0521d5e8
TheYusa <bilalyusaozer@gmail.com>
2026-04-09 03:58:52 +0330

    fix(ui): reuse existing allow insecure strings

--- composeApp/src/commonMain/kotlin/fr/husi/ui/SettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/fr/husi/ui/SettingsScreen.kt
@@ -651,14 +651,14 @@ fun SettingsScreen(
                         SwitchPreference(
                             value = value,
                             onValueChange = { DataStore.globalAllowInsecure = it },
-                            title = { Text(stringResource(Res.string.allow_insecure_global)) },
+                            title = { Text(stringResource(Res.string.allow_insecure)) },
                             icon = {
                                 Icon(
                                     vectorResource(Res.drawable.security),
                                     null,
                                 )
                             },
-                            summary = { Text(stringResource(Res.string.allow_insecure_global_sum)) },
+                            summary = { Text(stringResource(Res.string.allow_insecure_sum)) },
                         )
                     }
                     platformSecurityOptions()
